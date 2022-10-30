e21dc476c1e6dde8463da7c0fcf0eb19d437f820
ahmedzidan <ahmedzidan@airasia.com>
2022-10-30 23:59:30 +0800

    elastic operator

---
+++ elastic-operator/install.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+function install_operator() {
+  helm repo add elastic https://helm.elastic.co
+  helm repo update
+  helm upgrade \
+    --install \
+    elastic-operator elastic/eck-operator \
+    --namespace elastic-operator \
+    --create-namespace \
+    --version v2.4.0 \
+    -f operator.yaml
+}
+
+function install_stack() {
+ kubectl apply -k .
+}
+
+install_operator
+install_stack
---
+++ elastic-operator/readme.md
@@ -0,0 +1,11 @@
+## Install elasticsearch using Elastic Cloud on Kubernetes Operator
+
+This cluster will have 3 master nodes as well as three data nodes
+
+You can simply run the install.sh script and everything will be fine 
+
+```
+./install.sh
+```
+
+### Details 
